f8489e0451ea473c05584195d06f8cd11731f91a
scriptingmaverick <khasshaik12@gmail.com>
2026-05-10 10:56:08 +0530

    Feat: Creates a not method for probability

--- src/problem_2/model/Probability.java
+++ src/problem_2/model/Probability.java
@@ -3,10 +3,10 @@ package problem_2.model;
 import java.util.Objects;
 
 public class Probability {
-  private final double percentage;
+  private final double probability;
 
-  private Probability(double percentage) {
-    this.percentage = percentage;
+  private Probability(double probability) {
+    this.probability = probability;
   }
 
   public static Probability create(double percentage) {
@@ -17,11 +17,15 @@ public class Probability {
   public boolean equals(Object o) {
     if (o == null || getClass() != o.getClass()) return false;
     Probability that = (Probability) o;
-    return Double.compare(percentage, that.percentage) == 0;
+    return Double.compare(probability, that.probability) == 0;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(percentage);
+    return Objects.hashCode(probability);
+  }
+
+  public Probability not() {
+    return new Probability(1 - probability);
   }
 }
--- test/problem_2/model/ProbabilityTest.java
+++ test/problem_2/model/ProbabilityTest.java
@@ -5,11 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProbabilityTest {
-  
+
   @Test
   void shouldRepresentProbability() {
     Probability probabilityOfGettingTails = Probability.create(0.5);
 
     assertEquals(probabilityOfGettingTails, Probability.create(0.5));
+  }
+
+  @Test
+  void shouldRepresentNotGettingProbability() {
+    Probability probabilityForNotGettingTails = Probability.create(0.5);
+
+    assertEquals(probabilityForNotGettingTails.not(), Probability.create(0.5));
   }
 }
