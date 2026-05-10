4eabdf8b084e693ae3bde8ab2b14ef6365bf718a
scriptingmaverick <khasshaik12@gmail.com>
2026-05-10 11:40:12 +0530

    Refactor: Removes the ceiling funcitonality, it will cause bugs.

--- test/problem_3/model/LengthTest.java
+++ test/problem_3/model/LengthTest.java
@@ -2,6 +2,8 @@ package problem_3.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 class LengthTest {
   @Test
   void shouldCompare2Lengths() {
@@ -17,5 +19,21 @@ class LengthTest {
     Length twoInches = Length.create(2, LengthUnit.INCH);
 
     assert (fiveCms.compare(twoInches));
+  }
+
+  @Test
+  void shouldCompareAndReturnTrueFor1CmAnd10MM() {
+    Length oneCm = Length.create(1, LengthUnit.CM);
+    Length temMm = Length.create(10, LengthUnit.MM);
+
+    assert (oneCm.compare(temMm));
+  }
+
+  @Test
+  void shouldCompareAndReturnFalseFor2CmAnd10MM() {
+    Length oneCm = Length.create(2, LengthUnit.CM);
+    Length temMm = Length.create(10, LengthUnit.MM);
+
+    assertFalse(oneCm.compare(temMm));
   }
 }
